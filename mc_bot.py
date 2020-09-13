99dbffb10ffe1933798a6f0e935d789fd7d9eb73
Drake Thomas <drake550@pm.me>
2020-09-13 19:35:09 -0400

    tp users, show banned and op users
    using embed shows banned users and the reason. oplist shows server operators

--- mc_bot.py
+++ mc_bot.py
@@ -1,7 +1,8 @@
-import discord, asyncio, os, time
+import discord, asyncio, os, time, json
 from discord.ext import commands, tasks
 from datetime import datetime
 
+mc_server_dir = '/mnt/c/Users/DT/Desktop/MC/server'
 with open('/home/slime/mc_bot_token.txt', 'r') as file:
     TOKEN = file.readline()
 
@@ -18,76 +19,102 @@ async def mc_command(command):
     os.system(f'tmux send-keys -t mcserver "{command}" ENTER')
 
 def format_args(args):
-    if not args: return "No reason given."
+    if not args: return "No reason given.."
     else: return ' '.join(args)
+
+def get_json(json_file):
+    with open(mc_server_dir + '/' + json_file) as file: 
+        return [i for i in json.load(file)]
 
 @bot.event
 async def on_ready():
     await bot.wait_until_ready()
-
-@bot.command(aliases=['say'])
-async def server_say(ctx, *msg):
-    await mc_command('/say ' + ' '.join(msg))
-    await ctx.send("Message Sent!")
-    sprint('Server said: ' + ' '.join(msg))
+    sprint("Bot ready.")
 
 @bot.command(aliases=['save'])
 async def server_save(ctx):
     await mc_command('/save-all')
-    await ctx.send("World Saved!")
-    sprint("World Saved.")
+    await ctx.send("World saved.")
 
-
-@bot.command(aliases=['kick'])
-async def server_kick(ctx, player, *reason):
-    reason = format_args(reason)
-    await mc_command(f'/say {player} will be kicked in 5s for: {reason}')
-    time.sleep(5)
-    await mc_command(f"/kick {player}")
-    await ctx.send(f"Kicked {player} for: {reason}")
-
-    sprint(f"Kicked {player} for: {reason}")
-
-@bot.command(aliases=['ban'])
-async def server_ban(ctx, player, *reason):
-    reason = format_args(reason)
-    await mc_command(f"/say {player} will be banned in 5s for: {reason}")
-    time.sleep(5)
-    await mc_command(f"/kick {player}")
-    await mc_command(f"/ban {player}")
-    await ctx.send(f"Banned {player} for {reason}")
-
-    sprint(f"Banned {player} for: {reason}")
-
-@bot.command(aliases=['pardon', 'unban'])
-async def server_pardon(ctx, player, *reason):
-    reason = format_args(reason)
-    await mc_command(f"/say {player} pardoned for: {reason}")
-    await mc_command(f"/pardon {player}")
-    await ctx.send(f"Pardoned {player} for: {reason}")
+@bot.command(aliases=['say'])
+async def server_say(ctx, *msg):
+    msg = format_args(msg)
+    await mc_command('/say ' + msg)
+    await ctx.send("Message sent.")
 
 @bot.command(aliases=['tell', 'msg', 'whisper'])
 async def server_tell(ctx, player, *msg):
     msg = format_args(msg)
     await mc_command(f"/tell {player} {msg}")
-    sprint(f"Messaged {player}: {msg}")
+    await ctx.send("Sent message to {player}.")
+
+# ========== Ban, Kick, Whitelist, etc
+@bot.command(aliases=['kick'])
+async def server_kick(ctx, player, *reason):
+    reason = format_args(reason)
+    await mc_command(f'/say {player} will be kicked in 5s for: {reason}.')
+    time.sleep(5)
+    await mc_command(f"/kick {player}")
+    await ctx.send(f"Kicked {player}.")
+
+@bot.command(aliases=['ban'])
+async def server_ban(ctx, player, *reason):
+    reason = format_args(reason)
+    await mc_command(f"/say {player} will be banned in 5s for: {reason}.")
+    time.sleep(5)
+    await mc_command(f"/kick {player}")
+    await mc_command(f"/ban {player} {reason}")
+    await ctx.send(f"Banned {player}.")
+
+@bot.command(aliases=['pardon', 'unban'])
+async def server_pardon(ctx, player, *reason):
+    reason = format_args(reason)
+    await mc_command(f"/say {player} pardoned for: {reason}.")
+    await mc_command(f"/pardon {player}")
+    await ctx.send(f"Pardoned {player} for: {reason}.")
+
+@bot.command(aliases=['banlist', 'blist'])
+async def server_ban_list(ctx):
+    embed = discord.Embed(title='Banned Players')
+    for player in [i for i in get_json('banned-players.json')]:
+        embed.add_field(name=player['name'], value=player['reason'])
+    await ctx.send(embed=embed)
+
+# ========== Player op, kill, tp, etc
+@bot.command(aliases=['oplist', 'showop', 'ops'])
+async def server_op_list(ctx):
+    op_players = [f"`{i['name']}`" for i in get_json('ops.json')]
+    await ctx.send('\n'.join(op_players))
 
 @bot.command(aliases=['kill', 'assassinate'])
 async def server_kill(ctx, player, *reason):
     reason = format_args(reason)
-    await mc_command(f"/say Killing {player} for: {reason}")
+    await mc_command(f"/say Killing {player} for: {reason}.")
     await mc_command(f'/kill {player}')
-    sprint(f"Killed {player} for: {reason}")
+    await ctx.send("Killed {player}")
 
 @bot.command(aliases=['delaykill', 'delayassassinate', 'dkill'])
-async def server_delay_kill(ctx, player, time=5, *reason):
+async def server_delay_kill(ctx, player, delay=5, *reason):
     reason = format_args(reason)
-    await mc_command(f"/say Killing {player} in {time}s for: {reason}")
-    sprint(f"Killing {player} in {time}s for: {reason}")
-    time.sleep(time)
+    await mc_command(f"/say Killing {player} in {delay}s for: {reason}.")
+    time.sleep(delay)
     await mc_command(f'/kill {player}')
-    sprint(f"Killed {player}")
+    await ctx.send(f"Killed {player}.")
 
+@bot.command(aliases=['tp', 'teleport'])
+async def server_teleport(ctx, player, target, *reason):
+    reason = format_args(reason)
+    await mc_command(f"/say Teleporting {player} to {target} in 5s for: {reason}")
+    time.sleep(5)
+    await mc_command(f"/tp {player} {target}")
+    await ctx.send(f"Teleported {player}.")
+
+@bot.remove_command("help")
+@bot.command(aliases=['help', 'h'])
+async def help_page(ctx):
+    embed = discord.Embed(title='Help')
+    embed.add_field(name='ban', value='`?ban <player>`\nAfter 5s bans player')
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
 
