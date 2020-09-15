fd3ffa64ef0d5b826c64c6603ec8b8a58b9ce0ff
Drake Thomas <drake550@pm.me>
2020-09-15 01:00:15 -0400

    changed start_server to mc_funcs

---
+++ mc_funcs.py
@@ -0,0 +1,21 @@
+#from bs4 import BeautifulSoup
+import shutil as su
+import os
+
+file_path = os.path.dirname(__file__)
+server_path = '/mnt/c/Users/DT/Desktop/MC/server'
+server_jar = f'{server_path}/server.jar'
+start_new_tmux = 'tmux new -d -s mcserver'
+run_args = f'java -Xmx2G -Xms1G -jar {server_jar} nogui java'
+start_server_command = f'tmux send-keys -t mcserver:1.0 "{run_args}" ENTER'
+
+def start_server():
+    os.chdir(server_path)
+    #tmux_status = os.system(start_tmux)
+    if not os.system(start_server_command):
+        return True
+
+if __name__ == '__main__':
+    start_server()
+
+
