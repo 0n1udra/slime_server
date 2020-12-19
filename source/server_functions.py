3c57005313b090734e7d633c7c9e97be0213f15a
0n1udra <dt01@pm.me>
2020-12-19 17:16:44 -0500

    moved stuff around. Now able to switch servers

--- source/server_functions.py
+++ source/server_functions.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 
 server_functions_path = os.getcwd()
 bot_token_file = '/home/slime/mc_bot_token.txt'
+bot_log_file = f"{server_functions_path}/bot_log.txt"
+new_server_url = 'https://www.minecraft.net/en-us/download/server'
+
+# ========== System Interfacing Options
+
+# Local file access allows for server files/folders manipulation for features like backup/restore world saves, editing server.properties file, and read server log.
+server_files_access = True
+
+# Use Tmux to send commands to server. You can disable Tmux and RCON to disable server control, and can just use files/folder manipulation features like world backup/restore.
+use_tmux = True
 
 # Run Minecraft server using subprocess.Popen(). Note, If script halts the server will halt also. Useful if you can't get Tmux, but I recommend Tmux if you can.
 # If use_tmux is also True, script will prioritize use_subprocess. Which means if both vars are True, MC server will run as subprocess instead of running in separate Tmux pane.
@@ -11,30 +21,35 @@ bot_token_file = '/home/slime/mc_bot_token.txt'
 use_subprocess = False
 
 # If you have local access to server files but not using Tmux, use RCON to send commands to server. You won't be able to use some features like reading server logs.
-use_rcon = False
 mc_ip = 'arcpy.asuscomm.com'
+use_rcon = False
 rcon_port = 25575
 rcon_pass = 'SlimeySlime'
-
-# Local file access allows for server files/folders manipulation for features like backup/restore world saves, editing server.properties file, and read server log.
-server_files_access = True
-# This is where Minecraft server, world backups and server backups will be saved, so make sure this is a full path and is where you want it.
-mc_path = '/mnt/c/Users/DT/Desktop/MC'
-
-# These don't have to be changed.
-server_path = f"{mc_path}/server"
-world_backups_path = f"{mc_path}/world_backups"
-server_backups_path = f"{mc_path}/server_backups"
-bot_log_file = f"{server_functions_path}/bot_log.txt"
-
-# Use Tmux to send commands to server. You can disable Tmux and RCON to disable server control, and can just use files/folder manipulation features like world backup/restore.
-use_tmux = True
-java_command = f'java -Xmx2G -Xms1G -jar {server_path}/server.jar nogui'  # Update server.jar execution argument for your setup if needed.
 
 if use_rcon: import mctools, re
 if server_files_access: import shutil, fileinput, json
 
-new_server_url = 'https://www.minecraft.net/en-us/download/server'
+# ========== Minecraft Server Config
+
+# This is where Minecraft server, world backups and server backups will be saved, so make sure this is a full path and is where you want it.
+mc_path = '/mnt/c/Users/DT/Desktop/MC'
+
+server_list = {'papermc': ["papermc", f'java -Xmx2G -Xms1G -jar {mc_path}/papermc/server.jar nogui', 'Lightweight PaperMC.'],
+               'vanilla': ["vanilla", f"java -Xmx2G -Xms1G -jar {mc_path}/vanilla/server.jar nogui", 'Plain old vanilla.'],
+               'valhesia3': ["valhesia3", f"{mc_path}/valhesia3/ServerStart.sh", "140 mods!, Note: Takes a long time to start."]
+               }
+
+server = server_list['papermc']
+if os.path.isfile(f"{server_functions_path}/vars.txt"):
+    with open(f"{server_functions_path}/vars.txt", 'r') as f:
+        server = server_list[f.read().strip()]
+
+
+server_path = f"{mc_path}/{server[0]}"
+
+world_backups_path = f"{mc_path}/world_backups/{server[0]}"
+server_backups_path = f"{mc_path}/server_backups/{server[0]}"
+
 mc_active_status = False
 mc_subprocess = None
 
@@ -48,6 +63,8 @@ def lprint(arg1=None, arg2=None):
     output = f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ({user}): {msg}"
     print(output)
     with open(bot_log_file, 'a') as file: file.write(output + '\n')
+
+lprint("Server selected: " + server[0])
 
 # ========== Server command, start, bot start.
 def mc_start():
@@ -65,13 +82,13 @@ def mc_start():
     os.chdir(server_path)
     if use_subprocess:
         # Runs MC server as subprocess. Note, If this script stops, the server will stop.
-        try: mc_subprocess = subprocess.Popen(java_command.split(), stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        try: mc_subprocess = subprocess.Popen(server[1].split(), stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
         except: lprint("Error server starting subprocess")
         if type(mc_subprocess) == subprocess.Popen: return True
     elif use_tmux:
         os.system('tmux send-keys -t mcserver:1.0 "cd /" ENTER')  # Fix: 'java.lang.Error: Properties init: Could not determine current working' error
         os.system(f'tmux send-keys -t mcserver:1.0 "cd {server_path}" ENTER')
-        if not os.system(f'tmux send-keys -t mcserver:1.0 "{java_command}" ENTER'): return True # Tries starting new detached tmux session.
+        if not os.system(f'tmux send-keys -t mcserver:1.0 "{server[1]}" ENTER'): return True # Tries starting new detached tmux session.
     else: return "Error starting server."
 
 # Sends command to tmux window running server.
