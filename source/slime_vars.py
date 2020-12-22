098f6a56d81426d903ab6fcb9432c81205cdc36d
0n1udra <dt01@pm.me>
2020-12-22 01:50:23 -0500

    moved some stuff around

--- source/slime_vars.py
+++ source/slime_vars.py
@@ -12,9 +12,8 @@ server_files_access = True
 # Use Tmux to send commands to server. You can disable Tmux and RCON to disable server control, and can just use files/folder manipulation features like world backup/restore.
 use_tmux = True
 
-# Run Minecraft server using subprocess.Popen(). Note, If script halts the server will halt also. Useful if you can't get Tmux, but I recommend Tmux if you can.
-# If use_tmux is also True, script will prioritize use_subprocess. Which means if both vars are True, MC server will run as subprocess instead of running in separate Tmux pane.
-# And if use_rcon is also True, RCON capabilities will have top priority over Popen() and Tmux.
+# Uses subprocess.Popen(). If script halts, server halts also. Useful if not using Tmux, recommend using Tmux if can.
+# Prioritize use_subprocess over Tmux option.
 use_subprocess = False
 
 # If you have local access to server files but not using Tmux, use RCON to send commands to server. You won't be able to use some features like reading server logs.
@@ -38,18 +37,22 @@ server_path = f"{mc_path}/{server[0]}"
 world_backups_path = f"{mc_path}/world_backups/{server[0]}"
 server_backups_path = f"{mc_path}/server_backups/{server[0]}"
 
-# ========== Misc
+# ========== Bot Config
 
 bot_log_file = f"{server_functions_path}/bot_log.txt"
 
 mc_active_status = False
 mc_subprocess = None
 
-mc_auto_saveall = False
-mc_auto_saveall_interval = 30
+# ===== Autosave
+autosave = False
+autosave_multiplier = 60  # 1: Seconds, 60: Minutes, 3_600: Hours
+autosave_interval = autosave_multiplier * 30  # Default autosave value, e.g. 30m (30 * 3_600)
 
 enable_inputs = ['enable', 'activate', 'true', 'on']
 disable_inputs = ['disable', 'deactivate', 'false', 'off']
 
-if use_rcon: import mctools, re
-if server_files_access: import shutil, fileinput, json
+if use_rcon:
+    import mctools, re
+if server_files_access:
+    import shutil, fileinput, json
