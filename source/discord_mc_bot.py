e242249d3432af55f7150edb4ff66b8e2f6318ea
0n1udra <dt01@pm.me>
2021-07-04 19:26:07 -0400

    4.1.1 minor changes

--- source/discord_mc_bot.py
+++ source/discord_mc_bot.py
@@ -4,7 +4,8 @@ import server_functions
 from discord_components import DiscordComponents, Button
 from server_functions import lprint, use_rcon, format_args, server_command, server_status
 
-__version__ = "4.1"
+__version__ = "4.1.1"
+__date__ = '7/4/2021'
 __author__ = "DT"
 __email__ = "dt01@pm.me"
 __license__ = "GPL 3"
