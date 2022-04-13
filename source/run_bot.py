6f8d765d88494a34f67432a0f1ee87d99781c29c
0n1udra <dt01@pm.me>
2022-04-13 16:59:22 -0400

    ig showlog argument doesn't work right with other argus *shrugs*

--- source/run_bot.py
+++ source/run_bot.py
@@ -112,6 +112,7 @@ def script_help():
     startserver - Start MC server.
     startboth   - Start Minecraft server and bot either using Tmux or in current console depending on corresponding variables.
     showlog     - Show bot log using 'watch -n X tail .../bot_log.txt' command. To get out of it, use ctrl + c.
+                  Use standalone, showlog will not work properly if used with other arguments.
     attachtmux  - Attaches to session. Will not start Tmux, use starttmux or setup.
     --nohup     - Run bot in background using nohup command. (stopbot will work without)
         E.g. python3 run_bot.py --nohup startbot
